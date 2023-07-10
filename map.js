1d69fe3343a77ca667b4438deb5c1b0c8a0648f5
Monique Parnis <monique.parnis@gmail.com>
2023-07-10 17:04:08 +0000

    Add Challenge

--- map.js
+++ map.js
@@ -52,3 +52,53 @@ const students = [
 const studentsWithIds = students.map(student => [student.name, student.id]);
 console.log(studentsWithIds);
 
+
+// Challenge - Use the destructuring assignment to create two variables and 
+// assign them values from calling the map method on the students array. 
+// The function provided to the map method can either be created within 
+// the map method or outside and passed in but in either case, it should 
+// use an arrow function. 
+// The map method should return an array for each item in the students array 
+// and this new array should contain 2 items: items name value and items results value.
+
+let collegeStudents = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+// Create variables named john and rest using the destructuring assignment
+// Assign the variable values from calling the map method on the collegestudents array
+// Within the map method use an arrow function
+let [john, ...rest] = collegeStudents.map(student => [student.name, student.results]);
+// Log out the variable named john to see its value
+console.log(john);
+// Log out the variable named rest to see its value
+console.log(rest);
+
+// The following provides a function created outside of the map method
+const mapper = (itm) => [itm.name, itm.results];
+let [John, ...Rest] = collegeStudents.map(mapper);
+console.log(John);
+console.log(Rest);
